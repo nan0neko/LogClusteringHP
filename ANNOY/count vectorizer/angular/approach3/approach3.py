d08a6448fe0851db33f82f7bb92c599b438d522b
Sushma <91407390+Sushma24-git@users.noreply.github.com>
2023-04-24 22:56:26 +0530

    Add files via upload

---
+++ ANNOY/count vectorizer/angular/approach3/approach3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv('E:\HPE-CTY\ANNOY\output2.csv')
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df['Message'])
+# X = vectorizer.fit_transform(df['Message']).toarray()
+
+#create annoy index
+from annoy import AnnoyIndex
+
+n_trees = 10
+n_dimensions = X.shape[1]
+
+index = AnnoyIndex(n_dimensions, metric='angular')
+
+for i in range(X.shape[0]):
+    v = X[i].toarray().flatten()
+    index.add_item(i, v)
+# for i,v in enumerate(X):
+#     index.add_item(i, v)
+
+index.build(n_trees)
+log_id = 3 #Index of the log to find nearest neighbor
+n_neighbors = 10
+
+# Get nearest neighbors
+similar_ids = index.get_nns_by_item(log_id, n_neighbors)
+similar_logs = df.iloc[similar_ids]
+
+print(similar_logs)
